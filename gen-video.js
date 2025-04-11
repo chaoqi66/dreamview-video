385d6906ea255ed1074707d1cc93365bc4ba4a3d
chaoqi.wang <chaoqi.wang@robosense.cn>
2025-04-11 11:52:04 +0800

    生成视频

--- gen-video.js
+++ gen-video.js
@@ -2,11 +2,11 @@ const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 const path = require('path');
 
-const outputFilename = 'output.mp4'; // 输出视频文件名
+const outputFilename = 'dreamview.mp4'; // 输出视频文件名
 
 // 使用 fluent-ffmpeg 开始构建视频
 ffmpeg()
-    .input(path.join(__dirname, 'output', 'frame-%05d.png'))
+    .input(path.join(__dirname, 'output/dreamview_video_frame', 'frame-%05d.png'))
     .inputFPS(10)
     .size('?x720')
     .output(path.join("./", outputFilename)) // 输出文件路径
